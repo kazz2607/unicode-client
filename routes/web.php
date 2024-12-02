0e504187734ef30c1b48d1b05baf1874d57c9cf0
kazz2607 <kairu2607@gmail.com>
2024-12-02 23:39:59 +0700

    Bai 148

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,44 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('unicode/redirect', function (Request $request){
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => '1',
+        'redirect_uri' => 'http://myweb.dev:8001/unicode/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+        // 'prompt' => '', // "none", "consent", or "login"
+    ]);
+
+    return redirect('http://unicode.dev:8000/oauth/authorize?'.$query);
+
+});
+
+Route::get('unicode/callback', function(Request $request){
+    if ($request->code){
+        $code = $request->code;
+        $state = $request->session()->pull('state');
+
+        $response = Http::asForm()->post('http://unicode.dev:8000/oauth/token', [
+            'grant_type' => 'authorization_code',
+            'client_id' => '1',
+            'client_secret' => '3UFWpQpAyTzQLyANaTMG6mrOEgEYKesP3RYe3NUf',
+            'redirect_uri' => 'http://myweb.dev:8001/unicode/callback',
+            'code' => $code,
+        ]);
+     
+        $response = $response->json();
+        if (!empty($response['access_token'])){
+            $accessToken = $response['access_token'];
+
+            $user = Http::withHeaders([
+                'Authorization' => 'Bearer '.$accessToken,
+            ])->get('http://unicode.dev:8000/api/user');
+        }
+    }
 });
